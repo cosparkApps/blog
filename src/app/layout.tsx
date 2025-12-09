31cf7fe9388ed725cd7d9fa714431a764b79fd6f
Sakuard <icycatsakura@gmail.com>
2025-12-09 21:00:39 +0800

    feat: optimize SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,12 +7,31 @@ const inter = Inter({
   subsets: ['latin'],
 });
 
+import { BASE_URL } from '@/config';
+
 export const metadata: Metadata = {
+  metadataBase: new URL(BASE_URL),
   title: {
     default: 'Matt の 小天地',
     template: '%s | Matt の 小天地',
   },
   description: '一個自我紀錄的小角落 - 有時技術，有時感悟',
+  openGraph: {
+    title: 'Matt の 小天地',
+    description: '一個自我紀錄的小角落 - 有時技術，有時感悟',
+    url: BASE_URL,
+    siteName: 'Matt の 小天地',
+    locale: 'zh_TW',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Matt の 小天地',
+    description: '一個自我紀錄的小角落 - 有時技術，有時感悟',
+  },
+  verification: {
+    google: '4yXQoexDZDdb70jGClBcTSfOHyJ333IWYCP5rr4I5Hg',
+  },
   icons: {
     icon: [
       { url: '/favicons/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
